a73bd0a263e79bda2ac54426b42de98d7b467239
ikd-gifu <oki19ki@gmail.com>
2021-06-23 22:37:18 +0900

    20210623_controller_devise_to_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  resources :users, :only => [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
