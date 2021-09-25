bbc2210e07719086ca09a7379f84e911c8dd6d25
ikd-gifu <oki19ki@gmail.com>
2021-09-25 22:59:49 +0900

    20210925_add_PlantBasicDatum_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_132707) do
+ActiveRecord::Schema.define(version: 2021_09_25_133354) do
+
+  create_table "plant_basic_data", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "plant_name", null: false
+    t.string "plant_breed_name"
+    t.string "plant_family_name"
+    t.string "plant_alias"
+    t.string "plant_scientific_name"
+    t.string "plant_place_of_origin"
+    t.text "characteristics_applications"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_plant_basic_data_on_user_id"
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_06_14_132707) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plant_basic_data", "users"
 end
