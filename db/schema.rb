c9aa7d348a395092ed17ed348383e2d09f2ef466
ikd-gifu <oki19ki@gmail.com>
2021-10-09 22:06:18 +0900

    20211009_error_resolved_and_user_show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_133354) do
+ActiveRecord::Schema.define(version: 2021_10_07_123917) do
 
   create_table "plant_basic_data", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "plant_name", null: false
