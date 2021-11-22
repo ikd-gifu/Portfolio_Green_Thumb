9da288a83cdaaa221a6009ec43cf7fcae2a70146
ikd-gifu <oki19ki@gmail.com>
2021-11-22 22:42:47 +0900

    20211122_PBD_modal_fade_deleted

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 // require("bootstrap/dist/js/bootstrap")
-//= require rails-ujs
+//= require jquery-ujs
 //= require turbolinks
 //= require jquery
 //= require bootstrap
